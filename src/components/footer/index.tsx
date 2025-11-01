7c3b749f298fd134666d866d06bf6eb2d279c0ee
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-11-01 20:24:14 +0300

    refactor: replace container with max-width utilities for footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ import { Input } from "../ui/input";
 export function Footer() {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 border-t">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {/* Newsletter Subscription */}
         <div className="flex justify-start mb-8">
           <div className="w-full md:w-1/2 lg:w-1/3">
